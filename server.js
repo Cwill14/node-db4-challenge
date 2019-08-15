380b5b30317af0bcaf3150ac1ab8a2537a9a372e
Clark Williams <theclarkwilliams@gmail.com>
2019-08-15 14:22:35 -0600

    data tables plotted, server up

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+// const  = require(''); router
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+server.use(logger)
+
+function logger(req, res, next) {
+    const method = req.method;
+    const url = req.url;
+    const timestamp = Date.now();
+    console.log(`${method} request to '${url}' at ${timestamp}`);
+    next()
+};
+
+module.exports = server;
